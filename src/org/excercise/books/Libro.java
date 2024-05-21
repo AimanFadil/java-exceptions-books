9878777cb95d17b8b0bedb78528cd1c16664fc12
AimanFadil <aimanbello2001@gmail.com>
2024-05-21 14:12:38 +0200

    Initial commit

---
+++ src/org/excercise/books/Libro.java
@@ -0,0 +1,26 @@
+package org.excercise.books;
+
+public class Libro {
+
+    //ATTRIBUTI
+
+    private String titolo;
+    private int nPagine;
+    private String autore;
+    private String editore;
+
+
+    //COSTRUTTORI
+
+
+    //METODI
+
+
+    //GETTER
+
+
+
+    //SETTER
+
+
+}
---
+++ src/org/excercise/books/Main.java
@@ -0,0 +1,7 @@
+package org.excercise.books;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
