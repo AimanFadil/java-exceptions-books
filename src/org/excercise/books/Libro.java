cb1628dc06ca796984eab53690210a74df2d5948
AimanFadil <aimanbello2001@gmail.com>
2024-05-21 15:45:12 +0200

    books

--- src/org/excercise/books/Libro.java
+++ src/org/excercise/books/Libro.java
@@ -12,15 +12,74 @@ public class Libro {
 
     //COSTRUTTORI
 
+    public Libro(String titolo, int nPagine, String autore, String editore) {
+        setTitolo(titolo);
+        setnPagine(nPagine);
+        setAutore(autore);
+        setEditore(editore);
+    }
+
 
     //METODI
+
+    @Override
+    public String toString() {
+        return "Libro" +
+                "titolo=" + titolo +
+                ", nPagine=" + nPagine +
+                ", autore=" + autore +
+                ", editore=" + editore
+                ;
+    }
 
 
     //GETTER
 
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public int getnPagine() {
+        return nPagine;
+    }
+
+    public String getAutore() {
+        return autore;
+    }
+
+    public String getEditore() {
+        return editore;
+    }
+
 
 
     //SETTER
+
+    public void setTitolo(String titolo) throws  IllegalArgumentException{
+        if(titolo == null || titolo.trim().isEmpty()){
+            throw new IllegalArgumentException("Il titolo non può essere vuoto.");
+        };
+        this.titolo = titolo;
+    }
+    public void setnPagine(int nPagine) throws  IllegalArgumentException{
+        if(nPagine <= 0){
+            throw new IllegalArgumentException("Il numero di pagine non può essere meno di 0.");
+        };
+        this.nPagine = nPagine;
+    }
+    public void setAutore(String autore) throws  IllegalArgumentException {
+        if(autore == null || autore.trim().isEmpty()){
+            throw new IllegalArgumentException("l'autore non può essere vuoto.");
+        };
+        this.autore = autore;
+    }
+
+    public void setEditore(String editore)  throws  IllegalArgumentException{
+        if(editore == null || editore.trim().isEmpty()){
+            throw new IllegalArgumentException("l'editore non può essere vuoto.");
+        };
+        this.editore = editore;
+    }
 
 
 }
--- src/org/excercise/books/Main.java
+++ src/org/excercise/books/Main.java
@@ -1,7 +1,52 @@
 package org.excercise.books;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Quanti libri vuoi inserire: ");
+        int quantita = scanner.nextInt();
+        scanner.nextLine();
+
+        Libro[] libro = new Libro[quantita];
+
+        for (int i = 0; i < quantita; i++) {
+            try {
+
+                    System.out.println("\nInserimento Libro " + (i + 1) + ":");
+
+                    System.out.print("Titolo: ");
+                    String titolo = scanner.nextLine();
+
+                    System.out.print("Numero Pagine: ");
+                    int nPagine = scanner.nextInt();
+
+                    scanner.nextLine();
+
+                    System.out.print("Autore: ");
+                    String autore = scanner.nextLine();
+
+                    System.out.print("Editore: ");
+                    String editore = scanner.nextLine();
+
+                    libro[i] = new Libro(titolo, nPagine, autore, editore);
+
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }finally {
+                System.out.println("fineeeeeeeeee");
+            }
+        }
+        System.out.println(Arrays.toString(libro));
+
+
+
+
 
     }
 }
